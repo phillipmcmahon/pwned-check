03fa3f5e33dae49a167f1081fa576684aefa0e5b
Phillip McMahon <phillipmcmahon@MacBookPro.phillipmcmahon.com>
2026-05-01 12:05:59 +0100

    Allow Fedora PAM libeconf dependency

--- scripts/native-pam-deps-check.sh
+++ scripts/native-pam-deps-check.sh
@@ -27,7 +27,7 @@ printf '%s\n' "$DEPS" | awk '
     /^[[:space:]]*linux-vdso/ { print $1; next }
 ' | while IFS= read -r dep; do
     case "$dep" in
-        ""|linux-vdso.so.*|ld-linux*.so.*|ld-musl-*.so.*|libc.so.*|libgcc_s.so.*|libdl.so.*|libpthread.so.*|libm.so.*|libpam.so.*|libaudit.so.*|libcap-ng.so.*)
+        ""|linux-vdso.so.*|ld-linux*.so.*|ld-musl-*.so.*|libc.so.*|libgcc_s.so.*|libdl.so.*|libpthread.so.*|libm.so.*|libpam.so.*|libaudit.so.*|libcap-ng.so.*|libeconf.so.*)
             ;;
         *)
             printf '%s\n' "$DEPS" >&2
